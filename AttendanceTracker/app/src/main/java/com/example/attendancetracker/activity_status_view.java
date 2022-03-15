e333e9d665499fd1d34af06c25dae227d7059395
patropez <87383961+patropez@users.noreply.github.com>
2022-03-15 16:32:04 +0800

    UI-Status-View-1st

--- AttendanceTracker/app/src/main/java/com/example/attendancetracker/activity_status_view.java
+++ AttendanceTracker/app/src/main/java/com/example/attendancetracker/activity_status_view.java
@@ -12,10 +12,6 @@ public class activity_status_view extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getSupportActionBar().hide();
-
         setContentView(R.layout.activity_status_view);
     }
 }
