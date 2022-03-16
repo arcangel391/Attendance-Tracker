afd0c7fbdfc443489f05c971517f132ae93a0390
jisuar <jsuarnaba.k11618065@umak.edu.ph>
2022-03-16 12:50:59 +0800

    navbar, so many changes im sorry if mali

---
+++ AttendanceTracker/app/src/main/java/com/example/attendancetracker/Navbar.java
@@ -0,0 +1,15 @@
+package com.example.attendancetracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Navbar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navbar);
+
+    }
+}
