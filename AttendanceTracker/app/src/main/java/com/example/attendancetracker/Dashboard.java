f5e9bd2dbe17e6f9469d308467296bc45763939d
jisuar <jsuarnaba.k11618065@umak.edu.ph>
2022-03-17 11:22:39 +0800

    navbar updated, fragment error

--- AttendanceTracker/app/src/main/java/com/example/attendancetracker/Dashboard.java
+++ AttendanceTracker/app/src/main/java/com/example/attendancetracker/Dashboard.java
@@ -2,12 +2,16 @@ package com.example.attendancetracker;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.MenuItem;
 import android.view.View;
 
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -32,7 +36,37 @@ public class Dashboard extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_dashboard2);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(navigationSelectedListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Dashboard()).commit();
+
     }
+
+    private BottomNavigationView.OnItemSelectedListener navigationSelectedListener = new BottomNavigationView.OnItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            Fragment selectedFragment = null;
+            switch (item.getItemId()){
+                case R.id.mainActivity:
+                    selectedFragment = new Dashboard();
+                    break;
+                case R.id.first2Fragment:
+                    selectedFragment = new First2Fragment();
+                    break;
+                case R.id.second2Fragment:
+                    selectedFragment = new Second2Fragment();
+                    break;
+                case R.id.fragment_third:
+                    selectedFragment = new fragment_third();
+                    break;
+            }
+
+            return true;
+        }
+    };
 
     @Override
     public boolean onSupportNavigateUp() {
--- AttendanceTracker/app/src/main/java/com/example/attendancetracker/Navbar.java
+++ AttendanceTracker/app/src/main/java/com/example/attendancetracker/Navbar.java
@@ -1,8 +1,13 @@
 package com.example.attendancetracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Navbar extends AppCompatActivity {
 
@@ -11,5 +16,11 @@ public class Navbar extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navbar);
 
+
     }
+
+
+
+
+
 }
