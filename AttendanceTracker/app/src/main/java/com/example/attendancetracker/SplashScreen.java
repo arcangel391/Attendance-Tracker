6110d128ca1ededc948802ea082f0f0bf1aac83a
mrtnsmpg <johnmartinsampaga@gmail.com>
2022-03-21 15:15:58 +0800

    splash screen finished

--- AttendanceTracker/app/src/main/java/com/example/attendancetracker/SplashScreen.java
+++ AttendanceTracker/app/src/main/java/com/example/attendancetracker/SplashScreen.java
@@ -1,19 +1,33 @@
 package com.example.attendancetracker;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.viksaa.sssplash.lib.activity.AwesomeSplash;
+import com.viksaa.sssplash.lib.cnst.Flags;
+import com.viksaa.sssplash.lib.model.ConfigSplash;
 
 public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getSupportActionBar().hide();
         setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, LoginPage.class);
+                startActivity(i);
+                finish();
+            }
+        }, 3000);
     }
+
 }
