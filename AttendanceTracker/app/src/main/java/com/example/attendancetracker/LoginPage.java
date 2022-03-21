9f096d77b0d1800d610b0c2fcc9f5a91752bbe8a
ccant1s <101537251+ccant1s@users.noreply.github.com>
2022-03-21 20:37:49 +0800

    Login -app dev team

--- AttendanceTracker/app/src/main/java/com/example/attendancetracker/LoginPage.java
+++ AttendanceTracker/app/src/main/java/com/example/attendancetracker/LoginPage.java
@@ -2,18 +2,130 @@ package com.example.attendancetracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginPage extends AppCompatActivity {
 
+    Button btnSignup;
+    TextView txtUsername, txtPass;
+    Button register,login;
+    CheckBox checkedStatus;
+    SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getSupportActionBar().hide();
         setContentView(R.layout.activity_login_page);
+        btnSignup = findViewById(R.id.btn_signin);
+        txtUsername = findViewById(R.id.txt_email);
+        txtPass = findViewById(R.id.txt_password);
+        checkedStatus = findViewById(R.id.checkbox);
+        sharedPreferences = getSharedPreferences("UserInfo", Context.MODE_PRIVATE);
+        String loginStatus = sharedPreferences.getString(getResources().getString(R.string.prefStatus),"");
+        if (loginStatus.equals("loggedin")){
+            startActivity(new Intent(LoginPage.this,Dashboard.class));
+            finish();
+
+
+
     }
+
+        //Button is pressed
+
+       btnSignup.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               String username = txtUsername.getText().toString();
+               String password = txtPass.getText().toString();
+
+               if(TextUtils.isEmpty(username)||TextUtils.isEmpty(password)){
+                   Toast.makeText(LoginPage.this, "All Fields Required", Toast.LENGTH_SHORT).show();
+
+
+               }
+               else{
+
+                   LoginMet(username,password);
+
+               }
+           }
+       });
+
+
+    }
+
+    private void LoginMet(final String username, final String password){
+        final ProgressDialog progressDialog = new ProgressDialog(LoginPage.this);
+        progressDialog.setTitle("");
+        progressDialog.setCancelable(false);
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.setIndeterminate(false);
+        progressDialog.show();
+        String uRl = "https://10.0.2.2/MelhamApp/login.php";
+        StringRequest request = new StringRequest(Request.Method.POST, uRl, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                if (response.equals("Login Success Welcome")){
+                    Toast.makeText(LoginPage.this, response, Toast.LENGTH_SHORT).show();
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    if (checkedStatus.isChecked()){
+                        editor.putString(getResources().getString(R.string.prefStatus),"loggedin");
+                    }
+                    else{
+                        editor.putString(getResources().getString(R.string.prefStatus),"loggedout");
+                    }
+                    editor.apply();
+                    startActivity(new Intent(LoginPage.this,Dashboard.class));
+                    progressDialog.dismiss();
+                    finish();
+                }
+                else {
+                    Toast.makeText(LoginPage.this, response, Toast.LENGTH_SHORT).show();
+                    progressDialog.dismiss();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(LoginPage.this, error.toString(), Toast.LENGTH_SHORT).show();
+                progressDialog.dismiss();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap<String,String> param = new HashMap<>();
+                param.put("username",username);
+                param.put("password",password);
+                return param;
+
+            }
+        };
+
+        request.setRetryPolicy(new DefaultRetryPolicy(30000,DefaultRetryPolicy.DEFAULT_MAX_RETRIES,DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        MySingleton.getmInstance(LoginPage.this).addToRequestQueue(request);
+    }
+
+
 }
