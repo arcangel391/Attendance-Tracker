a7fd2f0f2c82c756f3fcb50c8680ad268fc9e52d
ccant1s <101537251+ccant1s@users.noreply.github.com>
2022-04-01 23:26:43 +0800

    Dashboard 1 (Time)

--- AttendanceTracker/app/src/main/java/com/example/attendancetracker/Dashboard.java
+++ AttendanceTracker/app/src/main/java/com/example/attendancetracker/Dashboard.java
@@ -1,5 +1,8 @@
 package com.example.attendancetracker;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -23,15 +26,77 @@ import java.util.Date;
 
 public class Dashboard extends Fragment {
     TextView  day, date;
-    Button btnAttendance;
+    Button btnTime;
+    Context context;
+    String mess = "time in";
+    int nswitch = 0;
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        this.context =context;
+        btnTime = (Button)view.findViewById(R.id.btnAttendance);
+        btnTime.setOnClickListener(this::clicked );
 
-        btnAttendance = (Button)view.findViewById(R.id.btnAttendance);
+        message();
+
+
 
         return view;
+
+
+
+
     }
+
+    public void clicked (View v){
+
+
+
+    }
+
+
+
+
+    //method for btn color switching
+    public void clicked (Button btnTime1, int n ){
+
+
+        if (n==0){
+            btnTime1.setText("TIME OUT");
+            btnTime1.setBackgroundColor(Color.RED);
+            mess = "time-out";
+            nswitch = 1;
+
+        }
+        else{
+            btnTime1.setText("TIME IN");
+            btnTime1.setBackgroundColor(Color.GREEN);
+            mess = "time-in";
+            nswitch = 0;
+
+        }
+
+
+    }
+
+    public void message (){
+
+        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which){
+                    case DialogInterface.BUTTON_POSITIVE:
+                        clicked(btnTime,nswitch);
+                        break;
+
+                    case DialogInterface.BUTTON_NEGATIVE:
+
+                        break;
+                }
+            }
+        };
 
 }
