c536b2b7134bc9b2b286d057cf0bea33f3a0be7b
jisuar <jsuarnaba.k11618065@umak.edu.ph>
2022-04-04 11:12:35 +0800

    Ti To

--- AttendanceTracker/app/src/main/java/com/example/attendancetracker/Dashboard.java
+++ AttendanceTracker/app/src/main/java/com/example/attendancetracker/Dashboard.java
@@ -34,7 +34,7 @@ import java.util.Calendar;
 import java.util.Date;
 
 
-public class Dashboard extends Fragment {
+public class Dashboard extends Fragment implements View.OnClickListener{
     TextView  day, date, time;
     Button btnTime;
     Context context;
@@ -51,7 +51,7 @@ public class Dashboard extends Fragment {
         View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
         time = (TextView)view.findViewById(R.id.digitalClock);
         btnTime = (Button)view.findViewById(R.id.btnAttendance);
-        btnTime.setOnClickListener(this::clickTimeBtn );
+        btnTime.setOnClickListener(this);
        // listView= (ListView)view.findViewById(R.id.listAnnounceView);
 
         return view;
@@ -100,24 +100,32 @@ public class Dashboard extends Fragment {
 
 
 
-
+    @Override
     //method for btn color switching
-    public void clicked (Button btnTime1, int n ){
+    private void onClicked(View v){
 
-        if (n==0){
-            btnTime1.setText("TIME OUT");
-            btnTime1.setBackgroundColor(btnTime1.getContext().getResources().getColor(R.color.red));
+        if (v==btnTime) {
+            if (btnTime.getText().toString().equals("TIME IN")){
+                btnTime.setBackgroundColor(btnTime.getContext().getResources().getColor(R.color.red));
+                btnTime.setText("TIME OUT");
+            }
+            else{
+                btnTime.setBackgroundColor(btnTime.getContext().getResources().getColor(R.color.green));
+                btnTime.setText("TIME IN");
+            }
+            /*btnTime1.setText("TIME OUT");
+
             mess = "time-out";
-            nswitch = 1;
+            nswitch = 1;*/
 
         }
-        else{
+       /* else{
             btnTime1.setText("TIME IN");
             btnTime1.setBackgroundColor(btnTime1.getContext().getResources().getColor(R.color.green));
             mess = "time-in";
             nswitch = 0;
 
-        }
+        }*/
 
 
     }
