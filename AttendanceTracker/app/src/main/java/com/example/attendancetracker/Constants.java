10bae5511341ac539492d48e2255572f31c0f21e
mrtnsmpg <johnmartinsampaga@gmail.com>
2022-05-05 14:30:47 +0800

    ip changed

--- AttendanceTracker/app/src/main/java/com/example/attendancetracker/Constants.java
+++ AttendanceTracker/app/src/main/java/com/example/attendancetracker/Constants.java
@@ -2,7 +2,7 @@ package com.example.attendancetracker;
 
 public class Constants {
 
-    public static final String ROOT_URL = "http://duskish-releases.000webhostapp.com/MCC-AttendanceTracker/v1/";
+    public static final String ROOT_URL = "http://192.168.1.110/LocalHost_MCC-AttendanceTracker/v1/";
     public static final String URL_LOGIN = ROOT_URL+"loginFunction.php";
     public static final String URL_FORGOT_PASS = ROOT_URL+"forgot_password.php";
     public static final String URL_VERIFY_CODE = ROOT_URL+"verify_code.php";
